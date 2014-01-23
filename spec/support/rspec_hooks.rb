4e4a56cf7d99b93223b8b6d61ebb56b7d2d8d848
Travis Herrick <tthetoad@gmail.com>
2014-01-22 23:36:36 -0800

    Correct variable names in rspec_hooks support file

--- spec/support/rspec_hooks.rb
+++ spec/support/rspec_hooks.rb
@@ -1,8 +1,8 @@
 RSpec.configure do |config|
   # Disable all 'dangerous' actions.
   config.before do
-    save_util_methods = TestRunner::IO.methods - TestRunner::IO.superclass.methods
-    save_util_methods.each do |method|
+    io_methods = TestRunner::IO.methods - TestRunner::IO.superclass.methods
+    io_methods.each do |method|
       TestRunner::IO.stubs(method).raises NotImplementedError,
         "#{TestRunner::IO}.#{method} has been stubbed as not implemented."
     end
