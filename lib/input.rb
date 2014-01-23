ccd2d4001ffd83ae4c70041c8d99d197e1e8b01c
Travis Herrick <tthetoad@gmail.com>
2014-01-22 17:31:32 -0800

    Refactor TestRunner to be a module

--- lib/input.rb
+++ lib/input.rb
@@ -1,4 +1,4 @@
-class TestRunnerInput
+class TestRunner::Input
   class << self
     def read
       @file ||= open(File.expand_path('~/.triggertest'), 'r+')
--- lib/test_runner.rb
+++ lib/test_runner.rb
@@ -1,32 +1,30 @@
-require_relative 'input'
+module TestRunner
+  def self.run
+    args = TestRunner::Input.read.gets
 
-class TestRunner
-  class << self
-    def run
-      args = TestRunnerInput.read.gets
+    if args.strip == ''
+      args = @previous
+    else
+      @previous = args
+    end
 
-      if args.strip == ''
-        args = @previous
-      else
-        @previous = args
+    commands = { 'spec' => 'rspec', 'features' => 'cucumber' }
+    fname = linenum = command = nil
+
+    matched = args.match /^(spec|features)\// 
+    if matched
+      unless args =~ /step_definitions/
+        fname, linenum = args.split
+        command = commands[matched[1]]
       end
+    end
 
-      commands = { 'spec' => 'rspec', 'features' => 'cucumber' }
-      fname = linenum = command = nil
-
-      matched = args.match /^(spec|features)\// 
-      if matched
-        unless args =~ /step_definitions/
-          fname, linenum = args.split
-          command = commands[matched[1]]
-        end
-      end
-
-      if fname
-        cmd = "bundle exec #{command} #{fname} -l #{linenum}"
-        puts cmd
-        system cmd
-      end
+    if fname
+      cmd = "bundle exec #{command} #{fname} -l #{linenum}"
+      puts cmd
+      system cmd
     end
   end
 end
+
+require_relative 'input'
