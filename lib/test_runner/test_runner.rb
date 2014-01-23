7add21accc5736cf4f85770096a0ec8c2518796e
Travis Herrick <tthetoad@gmail.com>
2014-01-22 23:00:53 -0800

    Remove deprecated code in TestRunner

--- lib/test_runner/test_runner.rb
+++ lib/test_runner/test_runner.rb
@@ -12,30 +12,4 @@ module TestRunner
       @command = TestRunner::Command.new(args)
     end
   end
-
-  def self.run_archive
-    args = TestRunner::IO.input.gets
-
-    if args.strip == ''
-      args = @previous
-    else
-      @previous = args
-    end
-
-    commands = { 'spec' => 'rspec', 'features' => 'cucumber' }
-
-    matched = args.match /^(spec|features)\//
-    if matched
-      unless args =~ /step_definitions/
-        fname, linenum = args.split
-        command = commands[matched[1]]
-      end
-    end
-
-    if fname
-      cmd = "bundle exec #{command} #{fname} -l #{linenum}"
-      puts cmd
-      system cmd
-    end
-  end
 end
