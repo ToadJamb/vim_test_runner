c3432fef8b9465049f811b270b3d046c112b1dc0
Travis Herrick <tthetoad@gmail.com>
2014-01-23 00:43:45 -0800

    Exclude line number if run from the first line of the spec

--- lib/test_runner/command.rb
+++ lib/test_runner/command.rb
@@ -1,16 +1,29 @@
 class TestRunner::Command
   def initialize(args)
     @file, @line = args.split
+    set_line
   end
 
   def command
-    case File.extname(@file)
-    when '.rb'
-      "bundle exec rspec #{@file} -l #{@line}"
-    when '.feature'
-      "bundle exec cucumber #{@file} -l #{@line} -r features"
-    when '.lua'
-      "lspec #{@file} -l #{@line}"
+    mask = masks[File.extname(@file)[1..-1].to_sym]
+    mask.gsub(/%f/, @file).gsub(/%l/, @line) if mask
+  end
+
+  private
+
+  def masks
+    {
+      :rb      => 'bundle exec rspec %f %l',
+      :feature => 'bundle exec cucumber %f %l -r features',
+      :lua     => 'lspec %f %l',
+    }
+  end
+
+  def set_line
+    if @line == '1'
+      @line = ''
+    else
+      @line = "-l #{@line}"
     end
   end
 end
--- spec/lib/test_runner/command_spec.rb
+++ spec/lib/test_runner/command_spec.rb
@@ -17,27 +17,63 @@ describe TestRunner::Command do
 
     context 'given a ruby file' do
       let(:command) { "bundle exec rspec #{file} -l #{line}" }
+
       before { expect(File.extname(file)).to eq '.rb' }
+
       it 'invokes rspec' do
         expect(subject).to eq command
+      end
+
+      context 'given the first line' do
+        let(:line) { 1 }
+
+        before { expect(line).to eq 1 }
+
+        it 'does not include the line number' do
+          expect(subject).to_not match(/-l #{line}/)
+        end
       end
     end
 
     context 'given a feature file' do
       let(:command) { "bundle exec cucumber #{file} -l #{line} -r features" }
       let(:file)    { 'cucumber.feature' }
+
       before { expect(File.extname(file)).to eq '.feature' }
+
       it 'invokes cucumber' do
         expect(subject).to eq command
+      end
+
+      context 'given the first line' do
+        let(:line) { 1 }
+
+        before { expect(line).to eq 1 }
+
+        it 'does not include the line number' do
+          expect(subject).to_not match(/-l #{line}/)
+        end
       end
     end
 
     context 'given a lua file' do
       let(:command) { "lspec #{file} -l #{line}" }
       let(:file)    { 'file_spec.lua' }
+
       before { expect(File.extname(file)).to eq '.lua' }
+
       it 'invokes lspec' do
         expect(subject).to eq command
+      end
+
+      context 'given the first line' do
+        let(:line) { 1 }
+
+        before { expect(line).to eq 1 }
+
+        it 'does not include the line number' do
+          expect(subject).to_not match(/-l #{line}/)
+        end
       end
     end
   end
