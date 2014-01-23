d81aed174c5944665d7fc95c11e606129c17931b
Travis Herrick <tthetoad@gmail.com>
2014-01-22 18:51:16 -0800

    Rearrange files

--- lib/test_runner.rb
+++ lib/test_runner.rb
@@ -1,29 +1,5 @@
 module TestRunner
-  def self.run
-    args = TestRunner::Input.read.gets
-
-    if args.strip == ''
-      args = @previous
-    else
-      @previous = args
-    end
-
-    commands = { 'spec' => 'rspec', 'features' => 'cucumber' }
-
-    matched = args.match /^(spec|features)\//
-    if matched
-      unless args =~ /step_definitions/
-        fname, linenum = args.split
-        command = commands[matched[1]]
-      end
-    end
-
-    if fname
-      cmd = "bundle exec #{command} #{fname} -l #{linenum}"
-      puts cmd
-      system cmd
-    end
-  end
 end
 
-require_relative 'input'
+require_relative 'test_runner/test_runner'
+require_relative 'test_runner/input'
