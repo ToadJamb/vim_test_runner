73b95a3faef4996e4571dc9125159edde8c366cf
Travis Herrick <tthetoad@gmail.com>
2014-10-20 03:02:38 -0700

    Added Params

--- lib/test_runner.rb
+++ lib/test_runner.rb
@@ -5,6 +5,7 @@ require 'psych'
 
 require_relative 'test_runner/system'
 require_relative 'test_runner/exceptions'
+require_relative 'test_runner/params'
 require_relative 'test_runner/io'
 require_relative 'test_runner/command'
 require_relative 'test_runner/test_runner'
---
+++ spec/lib/test_runner/params_spec.rb
@@ -0,0 +1,69 @@
+require 'spec_helper'
+
+describe TestRunner::Params do
+  subject { instance }
+
+  let(:instance) { described_class.new arg }
+
+  let(:arg) { [file, line].join(' ') + "\n" }
+  let(:file) { '/path/to/file_spec.rb' }
+  let(:line) { rand(999) + 1 }
+
+  context '.valid?' do
+    subject { instance.valid? }
+
+    context 'given a non-empty string' do
+      it { expect(subject).to eq true }
+
+      context 'given additional checks' do
+        before { instance.valid? }
+        it { expect(subject).to eq true }
+      end
+    end
+
+    context 'given an empty string' do
+      let(:arg) { '' }
+      it { expect(subject).to eq false }
+    end
+
+    context 'given nil' do
+      let(:arg) { nil }
+      it { expect(subject).to eq false }
+    end
+
+    context 'given a blank string' do
+      let(:arg) { [' ', "\n"].sample }
+      it { expect(subject).to eq false }
+    end
+  end
+
+  context '.file' do
+    subject { instance.file }
+
+    context 'given a file' do
+      before { expect(arg).to_not be_empty }
+      it 'returns the file' do
+        expect(subject).to eq file
+      end
+    end
+  end
+
+  context '.line' do
+    subject { instance.line }
+
+    context 'given a file and a line' do
+      before { expect(arg).to match(/\S+ \d+/) }
+      it 'returns the file' do
+        expect(subject).to eq line.to_s
+      end
+    end
+
+    context 'given only a file' do
+      let(:line) { nil }
+      before { expect(arg).to match(/\D/) }
+      it 'returns nil' do
+        expect(subject).to eq nil
+      end
+    end
+  end
+end
